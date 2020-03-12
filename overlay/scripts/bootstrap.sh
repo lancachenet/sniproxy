64275900364ebe39b158aaa4ea5b9d02c107e0c3
Goten Xiao <me@gotenxiao.co.uk>
2020-03-12 17:12:51 +0000

    Fix #13 - Correctly support multiple upstream DNS

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Preprocess UPSTREAM_DNS to allow for multiple resolvers using the same syntax as lancache-dns
+export UPSTREAM_DNS="$(echo -n "${UPSTREAM_DNS}" | sed 's/[;]/ /g')"
+
 sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/sniproxy.conf
 
 
